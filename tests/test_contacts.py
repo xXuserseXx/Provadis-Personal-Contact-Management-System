d3754ed28c56dc859582955e79d6ac1d99697a66
linu <jahn.linus22@gmail.com>
2026-02-05 09:43:00 +0100

    wrote a simple unit test for constructors

---
+++ tests/test_contacts.py
@@ -0,0 +1,8 @@
+from ContactManagement import Contact, ContactManager, EmergencyContact, PersonalContact, WorkContact
+
+def construct_contact_classes():
+    Contact()
+    ContactManager()
+    EmergencyContact()
+    PersonalContact()
+    WorkContact()
